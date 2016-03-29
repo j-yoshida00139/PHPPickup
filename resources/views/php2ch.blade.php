d69dc35246110208d7256062bc426e504622bcdd
Jun Yoshida <yoshida@PC-2.local>
2016-03-30 00:26:23 +0200

    add crawled php page

---
+++ resources/views/php2ch.blade.php
@@ -0,0 +1,29 @@
+<div id="catch_contents">
+<?php
+    include_once(getcwd().'/../resources/views/simple_html_dom.php');
+    $curl = curl_init(); 
+    curl_setopt($curl, CURLOPT_URL, 'http://find.2ch.sc/?STR=php&TYPE=TITLE&x=29&y=9&BBS=ALL&ENCODING=SJIS&COUNT=50');  
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);  
+    $str = curl_exec($curl); 
+    curl_close($curl); 
+    $html= str_get_html($str);
+        ////////////////////　↓↓ここで取りたい要素を指定 ////////////////////
+    foreach($html -> find('dl dt a') as $list){
+       ////////////////////  ここから表示部分////////////////////////////////
+            echo "<ul class='news'>"; ///////任意のクラスを付けている
+            echo "<li class='news'>";
+            echo "<p>";
+            echo "<a href='";
+            echo $list->href;
+            echo "'>";
+            echo $list->outertext;
+            echo "</a>";
+            echo "</p>";
+            echo "</li>";
+            echo "</ul>";
+    }
+    $html -> clear();
+    unset($html);
+?>
+</div>
