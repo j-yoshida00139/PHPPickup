5616b5b1c63658d1c292d1a9a622c2c6ff87d270
Jun Yoshida <example@PC-2.local>
2016-04-03 07:56:01 +0200

    add description and keywords in layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="description" itemprop="description" content="2ちゃんねるのPHP関連最新情報のまとめ。プログラミング言語・開発環境・フレームワーク等の議論が中心。" />
+    <meta name="keywords" content="PHP,2ちゃんねる,最新情報,プログラミング,開発環境,フレームワーク">
     <title>PHP速報</title>
 
     <!-- Fonts -->
